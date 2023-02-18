9116317380940826466498fe5cc1a8a028f059f4
Viacheslav <jbc2txhst@gmail.com>
2023-02-18 18:11:03 +0500

    third commit

--- js/script.js
+++ js/script.js
@@ -44,11 +44,31 @@
 
 // alert(`Привет, ${user}`);
 
-let incr = 10;
-    decr = 10;
+// let incr = 10;
+//     decr = 10;
 
-incr++;
-decr--;
+// incr++;
+// decr--;
 
-console.log(incr);
-console.log(decr);
+// console.log(incr);
+// console.log(decr);
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
